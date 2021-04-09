1efee61c4fc2fe090d48b68d2bffa5ab95c4bcf7
elthondsaraujo <78942062+elthondsaraujo@users.noreply.github.com>
2021-04-09 17:51:16 -0300

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -15,6 +15,8 @@ apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
+wget https://cutt.ly/dcTz2ks
+unzip dcTz2ks
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
@@ -25,19 +27,20 @@ sudo apt-get install terminator
 sudo apt-get install gnome-terminal
 sudo apt-get install vlc
 sudo apt-get install ffmpeg
-cd home
-wget https://cutt.ly/dcTz2ks
-unzip dcTz2ks
+sudo apt-get update
+sudo wget https://sourceforge.net/projects/xdman/files/xdm-2018-x64.tar.xz
+tar -xvf xdm-2018-x64.tar.xz
+sudo ./install.sh
 curl https://rclone.org/install.sh | sudo bash
-rclone rcd --rc-web-gui --rc-user=user --rc-pass=user --rc-serve
 sudo adduser elton chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-read -p "Paste Here: " CRP
+read -p "Cole aqui o Codigo Debian: " CRP
 su - elton -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
+rclone rcd --rc-web-gui --rc-user=user --rc-pass=user --rc-serve
 if sudo apt-get upgrade &> /dev/null
 then
     printf "\n\nUpgrade Completed " >&2
